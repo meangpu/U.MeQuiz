26023a0f366cce85b070461ddb568267869be7d8
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-27 19:19:46 +0700

    Update QuizStateManager.cs

--- Runtime/QuizStateManager.cs
+++ Runtime/QuizStateManager.cs
@@ -24,6 +24,7 @@ namespace Meangpu.QuizExam
         {
             State = newState;
             OnUpdateGameState?.Invoke(newState);
+            OnUpdateGameStateEvent?.Invoke();
         }
     }
 
