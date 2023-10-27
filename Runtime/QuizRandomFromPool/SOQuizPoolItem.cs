b15022e7262680464a41978aa184fb82a75b9abe
meangpu <51825872+meangpu@users.noreply.github.com>
2023-10-27 23:04:44 +0700

    rename so create

--- Runtime/QuizRandomFromPool/SOQuizPoolItem.cs
+++ Runtime/QuizRandomFromPool/SOQuizPoolItem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Meangpu.QuizExam
 {
+    // the creation should be [CreateAssetMenu(menuName = "Meangpu/SOQuiz/...thisObjectName")]
     public abstract class SOQuizPoolItem : ScriptableObject
     {
         [TextArea]
--- Runtime/QuizSpecificChoice/SOQuizExam.cs
+++ Runtime/QuizSpecificChoice/SOQuizExam.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Meangpu.QuizExam
 {
-    [CreateAssetMenu(menuName = "Meangpu/SOQuizExam")]
+    [CreateAssetMenu(menuName = "Meangpu/SOQuiz/QuizExam")]
     public class SOQuizExam : ScriptableObject
     {
         public List<QuizObject> QuestionList;
